e52e23492bec5db96946fe1b6db6540986540ac6
superkookai <weerawut@live.com>
2025-11-11 15:48:44 +0700

    Initial Commit

---
+++ SwiftQuizFoundationModel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftQuizFoundationModel
+//
+//  Created by Weerawut Chaiyasomboon on 11/11/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftQuizFoundationModel/SwiftQuizFoundationModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuizFoundationModelApp.swift
+//  SwiftQuizFoundationModel
+//
+//  Created by Weerawut Chaiyasomboon on 11/11/2568.
+//
+
+import SwiftUI
+
+@main
+struct SwiftQuizFoundationModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
